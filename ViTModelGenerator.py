fixed test loss calculation

ViTModelGenerator.py
@@ -166,10 +166,6 @@ class ViTRegression(nn.Module):
                         target = angles.unsqueeze(1).float()
                         loss = self.criterion(output, target)  # L1 loss for regression applications
 
-                    # """ introduce regularization """
-                    # if self.wandb_config['l2_regularization_weight']:
-
-
                     loss.backward()
                     self.optimizer.step()
                     running_loss_train.append(loss.item())
@@ -184,8 +180,15 @@ class ViTRegression(nn.Module):
                     imgs, angles, vels = imgs.to(self.device), angles.to(self.device), vels.to(self.device)
                     self.optimizer.zero_grad()
                     output = self.forward(imgs)
-                    target = angles.unsqueeze(1).float()
-                    loss = self.criterion(output, target)  # L1 loss for regression applications
+
+                    no_outputs = self.mlp_head._modules['2'].out_features
+                    if no_outputs == 2:
+                        target = torch.cat((angles.unsqueeze(1).float(), vels.unsqueeze(1).float()), 1)
+                        loss = self.criterion(output, target)  # L1 loss for regression applications
+                    else:
+                        target = angles.unsqueeze(1).float()
+                        loss = self.criterion(output, target)  # L1 loss for regression applications
+
                     running_loss_test.append(loss.item())
                     example_ct_test += len(imgs)
                     batch_ct_test += 1